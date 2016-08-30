b45e632bccac40e5d5cf7c57041059fffec7bd80
Caio Gondim <caio.gondim@booking.com>
2016-08-30 20:48:36 +0200

    Refactor to handle all cases in a simpler way.
    Should be easier to create tests from now on

--- src/index.js
+++ src/index.js
@@ -1,50 +1,27 @@
 'use strict'
 
-// FIXME(caiogondim): A hell of an unrealiable hack!
-// const someIsPromise = (funcs) => {
-//   var flag = false
-//
-//   funcs.forEach(func => {
-//     if (func.toString().indexOf('new Promise') !== -1) {
-//       flag = true
-//     }
-//   })
-//
-//   return flag
-// }
-
 const isThenable = (obj) => {
   return typeof obj === 'object' && typeof obj.then === 'function'
 }
 
-const pipeSync = (result, ...funcs) => {
-  return funcs.reduce((value, func) => {
-    return func(value)
-  }, result)
-}
+const pipe = (...procs) => {
+  var output
 
-const pipeAsync = (...funcs) => {
-  var promise
-
-  funcs.forEach((func, i) => {
-    if (i === 0) {
-      promise = func.then()
+  procs.forEach((proc, i) => {
+    if (isThenable(output)) {
+      output = output.then(proc)
+    } else if (typeof proc === 'function') {
+      if (i === 0) {
+        output = proc()
+      } else {
+        output = proc(output)
+      }
     } else {
-      promise = promise.then(func)
+      output = proc
     }
   })
 
-  return promise
-}
-
-const pipe = (result, ...funcs) => {
-  if (typeof result === 'function') {
-    return pipe(result(), ...funcs)
-  } else if (isThenable(result)) {
-    return pipeAsync(result, ...funcs)
-  } else {
-    return pipeSync(result, ...funcs)
-  }
+  return output
 }
 
 module.exports = pipe
